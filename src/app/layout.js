89a926550cd4d9f9e7dd0101ea52bc3958343b6f
aries grey <ariesgrey88@gmail.com>
2023-11-08 20:10:41 -0600

    created Footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { cx } from "../utils";
 import "./globals.css";
 import { Inter, Manrope } from "next/font/google";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
 				className={cx(inter.variable, manrope.variable, "font-mr bg-light")}>
 				<Header />
 				{children}
+				<Footer />
 			</body>
 		</html>
 	);
