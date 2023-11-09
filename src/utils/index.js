b15fe542af82a6db4c748d8f4541c10754fd6162
aries grey <ariesgrey88@gmail.com>
2023-11-08 18:38:28 -0600

    created HomeCoverSection

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,13 @@
-/* Helper function for classNames */
+import { compareDesc, parseISO } from "date-fns";
+
+/* Helper function for applying classNames */
 export const cx = (...classNames) => classNames.filter(Boolean).join(" ");
+
+/* Sorts blogs by 'publishedAt' date - latest to oldest */
+export const sortBlogs = (blogs) => {
+	return blogs
+		.slice()
+		.sort((a, b) =>
+			compareDesc(parseISO(a.publishedAt), parseISO(b.publishedAt))
+		);
+};
