56931dcf5332b85b104ebad210905ea7eeadf3ff
aries grey <ariesgrey88@gmail.com>
2023-11-14 21:59:46 -0600

    added key to insights

--- src/components/About/InsightRoll.js
+++ src/components/About/InsightRoll.js
@@ -4,8 +4,8 @@ const InsightRoll = ({ insights }) => {
 	return (
 		<div className="w-full bg-accent text-light whitespace-nowrap overflow-hidden">
 			<div className="animate-roll w-full py-3 flex items-center justify-center capitalize font-semibold tracking-wider text-base">
-				{insights.map((text) => (
-					<div>
+				{insights.map((text, index) => (
+					<div key={index}>
 						{text} <span className="px-4">|</span>
 					</div>
 				))}
