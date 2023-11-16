ff54c30f5b1f645ac7491c9d071946599ad444b1
aries grey <ariesgrey88@gmail.com>
2023-11-15 20:28:11 -0600

    update siteUrl in siteMetaData

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -5,7 +5,7 @@ const siteMetaData = {
 	description: "A blog created with Next.js, Tailwind.css and contentlayer.",
 	language: "en-us",
 	theme: "system", // system, dark or light
-	siteUrl: "https://create-blog-with-nextjs.vercel.app", // your website URL
+	siteUrl: "https://nextjs-contentlayer-blog-rho.vercel.app/", // your website URL
 	siteLogo: "/logo.png",
 	socialBanner: "/social-banner.png", // add social banner in the public folder
 	email: "codebucks27@gmail.com",
